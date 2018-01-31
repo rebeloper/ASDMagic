6ce91f6567a00a7f3832aa65d9d3e740e93dca5f
Alex <therebeloper@gmail.com>
2018-01-31 09:28:35 +0200

    created branch

--- ASDMagic/ViewController.swift
+++ ASDMagic/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     print("First Print Statement")
+    view.backgroundColor = .red
   }
 
   override func didReceiveMemoryWarning() {
