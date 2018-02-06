acb152f55db34e77f4bc9fce535385d394160547
Alex <therebeloper@gmail.com>
2018-02-06 09:05:03 +0200

    safeAreaLayoutGuide

--- ASDMagic/ViewController.swift
+++ ASDMagic/ViewController.swift
@@ -9,13 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  let customView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .red
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     print("First Print Statement")
-    let backgroundColor = UIColor.purple
+    let backgroundColor = UIColor.white
     view.backgroundColor = backgroundColor
+    
+    view.addSubview(customView)
+    
+    customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    customView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+    
+    let width: CGFloat = 60
+    customView.widthAnchor.constraint(equalToConstant: width).isActive = true
+    customView.heightAnchor.constraint(equalToConstant: width).isActive = true
     
   }
 
